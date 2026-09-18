9083373b8928126b124d849fe63c5387f9fc7ee1
Mira and Mia <morteva@proton.me>
2026-09-18 17:33:15 -0500

    Use gothic Tiny Mia mascot asset

--- public/mia-pet.js
+++ public/mia-pet.js
@@ -196,7 +196,7 @@
   sprite.title = 'Click for another Tiny Mia thought';
 
   const img = document.createElement('img');
-  img.src = asset('tiny-mia-static.png?v=cb5d4728f06f');
+  img.src = asset('tiny-mia-gothic.png?v=1e36df66b9e8');
   img.alt = '';
   img.setAttribute('aria-hidden', 'true');
   sprite.append(img);
